b6a1aa7a8097ceedbea2d6fc560f368916cce295
Luca Del Puppo <luca@delpuppo.net>
2017-12-03 22:16:19 +0100

    Step 3: createSelector and createFeatureSelector

--- src/products/store/reducers/index.ts
+++ src/products/store/reducers/index.ts
@@ -1,7 +1,8 @@
 
-import { ActionReducerMap } from '@ngrx/store'
+import { ActionReducerMap, createFeatureSelector, createSelector } from '@ngrx/store'
 
 import * as fromPizzas from './pizza.reducers';
+import { getPizzas } from './pizza.reducers';
 
 export interface ProductsState {
   pizzas: fromPizzas.PizzaState
@@ -10,3 +11,16 @@ export interface ProductsState {
 export const reducers: ActionReducerMap<ProductsState> = {
   pizzas: fromPizzas.reducer
 }
+
+export const getProductState = createFeatureSelector<ProductsState>(
+  'products'
+);
+
+export const getPizzaState = createSelector(
+  getProductState,
+  (state: ProductsState) => state.pizzas
+);
+
+export const getAllPizzas = createSelector(getPizzaState, fromPizzas.getPizzas);
+export const getPizzasLoading = createSelector(getPizzaState, fromPizzas.getPizzasLoading);
+export const getPizzasLoaded = createSelector(getPizzaState, fromPizzas.getPizzasLoaded);
--- src/products/store/reducers/pizza.reducers.ts
+++ src/products/store/reducers/pizza.reducers.ts
@@ -12,7 +12,48 @@ export interface PizzaState {
 }
 
 export const initialState: PizzaState = {
-  data: [],
+  data: [{
+    "name": "Seaside Surfin'",
+    "toppings": [
+      {
+        "id": 6,
+        "name": "mushroom"
+      },
+      {
+        "id": 7,
+        "name": "olive"
+      },
+      {
+        "id": 2,
+        "name": "bacon"
+      },
+      {
+        "id": 3,
+        "name": "basil"
+      },
+      {
+        "id": 1,
+        "name": "anchovy"
+      },
+      {
+        "id": 8,
+        "name": "onion"
+      },
+      {
+        "id": 11,
+        "name": "sweetcorn"
+      },
+      {
+        "id": 9,
+        "name": "pepper"
+      },
+      {
+        "id": 5,
+        "name": "mozzarella"
+      }
+    ],
+    "id": 2
+  }],
   loaded: false,
   loading: false
 };
@@ -47,4 +88,14 @@ export function reducer(
       return state
   }
 
+}
+
+export function getPizzasLoading(state: PizzaState): boolean {
+  return state.loading;
+}
+export function getPizzasLoaded(state: PizzaState): boolean {
+  return state.loaded;
+}
+export function getPizzas(state: PizzaState): Pizza[] {
+  return state.data;
 }
