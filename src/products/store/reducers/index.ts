6ba2c79c084e149b14116da76368bd743ffa2fb2
Luca Del Puppo <luca@delpuppo.net>
2017-12-06 23:00:24 +0100

    Step 8: Router State and selector composition

--- src/products/store/reducers/index.ts
+++ src/products/store/reducers/index.ts
@@ -1,5 +1,5 @@
 
-import { ActionReducerMap, createFeatureSelector, createSelector } from '@ngrx/store'
+import { ActionReducerMap, createFeatureSelector } from '@ngrx/store'
 
 import * as fromPizzas from './pizza.reducers';
 
@@ -13,27 +13,4 @@ export const reducers: ActionReducerMap<ProductsState> = {
 
 export const getProductState = createFeatureSelector<ProductsState>(
   'products'
-);
-
-export const getPizzaState = createSelector(
-  getProductState,
-  (state: ProductsState) => state.pizzas
-);
-
-export const getPizzasEntities = createSelector(
-  getPizzaState,
-  fromPizzas.getPizzasEntities
-);
-
-export const getAllPizzas = createSelector(getPizzasEntities, entities => {
-  return Object.keys(entities).map(id => entities[parseInt(id, 10)]);
-});
-
-export const getPizzasLoaded = createSelector(
-  getPizzaState,
-  fromPizzas.getPizzasLoaded
-);
-export const getPizzasLoading = createSelector(
-  getPizzaState,
-  fromPizzas.getPizzasLoading
 );
