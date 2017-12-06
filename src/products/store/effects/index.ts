81d872b96c31588be8f3bce702f4cf2909d880da
Luca Del Puppo <luca@delpuppo.net>
2017-12-06 23:50:07 +0100

    Step 11: Toppings Effect

--- src/products/store/effects/index.ts
+++ src/products/store/effects/index.ts
@@ -1,8 +1,7 @@
-import { PizzasEffects } from './pizza.effect';
+import { PizzasEffects } from './pizzas.effect';
+import { ToppingsEffects } from './toppings.effect';
 
+export const effects: any[] = [PizzasEffects, ToppingsEffects];
 
-export { PizzasEffects } from './pizza.effect';
-
-export const effects: any[] = [PizzasEffects];
-
-export * from './pizza.effect';
+export * from './pizzas.effect';
+export * from './toppings.effect';
---
+++ src/products/store/effects/toppings.effect.ts
@@ -0,0 +1,28 @@
+
+import { Injectable } from '@angular/core';
+
+import { Actions, Effect } from '@ngrx/effects';
+import { of } from 'rxjs/observable/of';
+import { map, catchError, switchMap } from 'rxjs/operators';
+
+import * as toppingsAction from '../actions/toppings.action';
+import * as fromServices from '../../services/toppings.service';
+
+@Injectable()
+export class ToppingsEffects {
+  constructor(
+    protected actions$: Actions,
+    protected toppingService: fromServices.ToppingsService
+  ) { }
+
+  @Effect()
+  loadToppings$ = this.actions$.ofType(toppingsAction.LOAD_TOPPINGS)
+  .pipe(
+    switchMap(() => {
+      return this.toppingService.getToppings().pipe(
+        map(toppings => new toppingsAction.LoadToppingsSuccess(toppings)),
+        catchError(error => of(new toppingsAction.LoadToppingsFail(error)))
+      );
+    })
+  )
+}
