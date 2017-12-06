fe938ed4b47f6a82759f782713931ebbb7a57165
Luca Del Puppo <luca@delpuppo.net>
2017-12-06 23:33:39 +0100

    Step 10: Toppings Reducer

--- src/products/store/reducers/index.ts
+++ src/products/store/reducers/index.ts
@@ -2,13 +2,16 @@
 import { ActionReducerMap, createFeatureSelector } from '@ngrx/store'
 
 import * as fromPizzas from './pizza.reducers';
+import * as fromToppings from './toppings.reducers';
 
 export interface ProductsState {
-  pizzas: fromPizzas.PizzaState
+  pizzas: fromPizzas.PizzaState,
+  toppings: fromToppings.ToppingsState,
 }
 
 export const reducers: ActionReducerMap<ProductsState> = {
-  pizzas: fromPizzas.reducer
+  pizzas: fromPizzas.reducer,
+  toppings: fromToppings.reducer,
 }
 
 export const getProductState = createFeatureSelector<ProductsState>(
